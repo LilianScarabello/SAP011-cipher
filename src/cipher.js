2a6524579dd19b4006b9a942942eaff8bfb50af1
LilianScarabello <lilianscarabello@gmail.com>
2023-08-15 13:00:25 -0300

    estilização pelo css, decode e encode e read.me

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
-const cipher = {
-  // ...
-};
+const cipher = { encode, decode };
+
+function encode(offset, text) {
+  if (typeof Number(offset) !== 'number') {
+    throw new TypeError("Deslocamento precisa ser um número")
+  }
+  if (typeof text !== 'string') {
+    throw new TypeError("Texto precisa ser uma string")
+  }
+  let mensagemfinal = ''
+  for (let i = 0; i < text.length; i++) {
+    const charcode = text.charCodeAt(i)
+    const charcodenewletter = ((charcode - 65 + Number(offset)) % 26) + 65;
+    const mensagem = String.fromCharCode(charcodenewletter);
+    mensagemfinal = mensagemfinal + mensagem
+    console.log(mensagem)
+  }
+
+  return mensagemfinal;
+}
+
+function decode(offset, text) {
+  if (typeof Number(offset) !== 'number') {
+    throw new TypeError("Deslocamento precisa ser um número")
+  }
+  if (typeof text !== 'string') {
+    throw new TypeError("Texto precisa ser uma string")
+  }
+  let mensagemfinal = ''
+  for (let i = 0; i < text.length; i++) {
+    const charcode = text.charCodeAt(i)
+    const charcodenewletter = ((charcode + 65 - Number(offset)) % 26) + 65;
+    const mensagemDec = String.fromCharCode(charcodenewletter);
+    mensagemfinal = mensagemfinal + mensagemDec
+    console.log(mensagemDec)
+
+  }
+  return mensagemfinal
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById("button-encode").addEventListener("click", function () {
+  const text = document.getElementById("letter").value
+  const offset = document.getElementById("deslocamento").value;
+  const resultado = cipher.encode(offset, text)
+  console.log(resultado)
+  document.getElementById("codedletter").innerHTML = resultado
+})
+document.getElementById("button-decode").addEventListener("click", function () {
+  const text = document.getElementById("codedletter").value
+  const offset = document.getElementById("deslocamento").value
+  const decmsg = cipher.decode(offset, text)
+  console.log(decmsg)
+  document.getElementById("decodedletter").innerHTML = decmsg;
+})
+
+console.log(cipher)
